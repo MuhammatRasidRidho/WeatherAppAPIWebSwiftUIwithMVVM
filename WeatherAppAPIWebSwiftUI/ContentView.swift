e5fe7896695274761fe456b16f280489b8febd90
Mas Rasid <rasid517@gmail.com>
2020-09-17 21:48:50 +0700

    Initial Commit

---
+++ WeatherAppAPIWebSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherAppAPIWebSwiftUI
+//
+//  Created by Muhammat Rasid Ridho on 17/09/20.
+//  Copyright © 2020 Muhammat Rasid Ridho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
